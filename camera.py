963c61c768bc86047a2ac785e96727a45ec85161
Javier Castro <jaca2899@gmail.com>
2020-12-08 15:09:57 -0500

    Moved the code for the `Camera` class into its own file.

---
+++ camera.py
@@ -0,0 +1,9 @@
+# A camera is what shows the scene to the world.
+class Camera:
+    def __init__(
+            self,
+            frame_height = None,
+            frame_width = None,
+            frame_rate = None,
+            ):
+        pass
--- janim.py
+++ janim.py
@@ -21,14 +21,3 @@ class Actor:
     # children.
     def __init__(self):
         pass
-
-
-# A camera is what shows the scene to the world.
-class Camera:
-    def __init__(
-            self,
-            frame_height = None,
-            frame_width = None,
-            frame_rate = None,
-            ):
-        pass
