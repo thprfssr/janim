59ff5e2a05db20d489b3cc69126e2074eb126149
Javier Castro <jaca2899@gmail.com>
2020-12-09 12:18:30 -0500

    Defined a `draw()` method for the `Circle` class.

--- geometry.py
+++ geometry.py
@@ -1,8 +1,20 @@
+from constants import *
 from janim import *
+from vector import *
+
+from math import tau
 
 
 
-class Circle(Actor):
-    def __init__(self, center = (0, 0), radius = 1):
+class Circle(Element):
+    def __init__(self, radius = 1, center = ORIGIN):
         self.center = center
         self.radius = radius
+
+    def draw(self, context):
+        x = self.center.x
+        y = self.center.y
+        r = self.radius
+        context.set_source_rgba(1, 1, 1, 1)
+        context.arc(x, y, r, 0, tau)
+        context.fill()
