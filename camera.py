5a90dcf9fc239ecf520181e754487a5884cc1fba
Javier Castro <jaca2899@gmail.com>
2020-12-09 11:10:16 -0500

    Fixed bug in `camera.py`.

--- camera.py
+++ camera.py
@@ -6,8 +6,8 @@ from constants import *
 # Default camera constants
 FRAME_HEIGHT    = 9
 FRAME_WIDTH     = 16
-PIXEL_HEIGHT    = 1920
-PIXEL_WIDTH     = 1080
+PIXEL_HEIGHT    = 1080
+PIXEL_WIDTH     = 1920
 FRAME_RATE      = 60
 FRAME_CENTER    = ORIGIN
 
@@ -20,8 +20,8 @@ class Camera:
             frame_width     = FRAME_WIDTH,
             frame_center    = FRAME_CENTER,
             frame_rate      = FRAME_RATE,
-            pixel_height    = PIXEL_HEIGHT,
             pixel_width     = PIXEL_WIDTH,
+            pixel_height    = PIXEL_HEIGHT,
             ):
         self.frame_height   = frame_height
         self.frame_width    = frame_width
@@ -31,12 +31,13 @@ class Camera:
         self.frame_center   = frame_center
 
         self.create_cairo_surface()
+        self.draw_background()
 
     def create_cairo_surface(self):
         self.surface = cairo.ImageSurface(
                 cairo.Format.ARGB32,
-                height  = self.pixel_height,
-                width   = self.pixel_width,
+                self.pixel_height,
+                self.pixel_width,
                 )
         self.context = cairo.Context(self.surface)
 
@@ -56,7 +57,13 @@ class Camera:
         pw = self.pixel_width
         ph = self.pixel_height
 
+        v = Vector(x, y)
+
         x_new = (x + fw/2) * pw/fw
         y_new = (-y + fh/2) * ph/fh
 
         return Vector(x_new, y_new)
+
+    def draw_background(self):
+        self.context.set_source_rgba(0, 0, 0, 1)
+        self.context.paint()
